oktetlabs/dpdk-pmd-ts

commit c8ea5bf7777526ace992316af40eebc3ae9a56bd

perf/perf_prologue: use the same environment as tests
Prepare prologue to collect information for all interfaces to
be used in performance testing.

Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

ts/perf/perf_prologue.c
@@ -109,7 +109,7 @@ int
 main(int argc, char **argv)
 {
     const char *extra_sfc_devargs = "stats_update_period_ms=0";
-    rcf_rpc_server *iut_rpcs = NULL;
+    rcf_rpc_server *iut_jobs_ctrl = NULL;
     tapi_env_host *iut_host = NULL;
     tapi_env_host *tst_host = NULL;
     const struct if_nameindex *iut_port = NULL;
@@ -122,7 +122,7 @@ main(int argc, char **argv)
     unsigned int i;
 
     TEST_START;
-    TEST_GET_PCO(iut_rpcs);
+    TEST_GET_PCO(iut_jobs_ctrl);
     TEST_GET_HOST(iut_host);
     TEST_GET_HOST(tst_host);
     TEST_GET_IF(iut_port);
@@ -135,17 +135,19 @@ main(int argc, char **argv)
     CHECK_RC(cfg_add_instance_str("/local:/dpdk:/dev_args:pci_fn:1924::", NULL,
                                   CFG_VAL(STRING, extra_sfc_devargs)));
 
-    memset(&dev_info, 0, sizeof(dev_info));
-    rpc_rte_eth_dev_info_get(iut_rpcs, iut_port->if_index, &dev_info);
+    CHECK_RC(tapi_rte_eal_init(&env, iut_jobs_ctrl, 0, NULL));
 
-    CHECK_RC(test_add_pci_fn_prop(iut_rpcs, iut_port,
+    memset(&dev_info, 0, sizeof(dev_info));
+    rpc_rte_eth_dev_info_get(iut_jobs_ctrl, iut_port->if_index, &dev_info);
+
+    CHECK_RC(test_add_pci_fn_prop(iut_jobs_ctrl, iut_port,
                                   "max_tx_queues", dev_info.max_tx_queues));
-    CHECK_RC(test_add_pci_fn_prop(iut_rpcs, iut_port,
+    CHECK_RC(test_add_pci_fn_prop(iut_jobs_ctrl, iut_port,
                                   "max_rx_queues", dev_info.max_rx_queues));
 
-    CHECK_RC(test_add_pci_fn_prop(iut_rpcs, iut_port,
+    CHECK_RC(test_add_pci_fn_prop(iut_jobs_ctrl, iut_port,
                                   "min_mtu", dev_info.min_mtu));
-    CHECK_RC(test_add_pci_fn_prop(iut_rpcs, iut_port,
+    CHECK_RC(test_add_pci_fn_prop(iut_jobs_ctrl, iut_port,
                                   "max_mtu", dev_info.max_mtu));
 
     for (i = 0; i < rpc_dpdk_tx_offloads_num; i++)
@@ -168,15 +170,15 @@ main(int argc, char **argv)
         }
     }
 
-    rpc_rte_eth_macaddr_get(iut_rpcs, iut_port->if_index, &iut_mac);
+    rpc_rte_eth_macaddr_get(iut_jobs_ctrl, iut_port->if_index, &iut_mac);
     te_asprintf(&iut_mac_str, TE_PRINTF_MAC_FMT,
                 TE_PRINTF_MAC_VAL(iut_mac.addr_bytes));
     CHECK_NOT_NULL(iut_mac_str);
     CHECK_RC(cfg_add_instance_fmt(NULL, CFG_VAL(STRING, iut_mac_str),
                                   "/local:/dpdk:/mac:%s", TEST_ENV_IUT_PORT));
 
-    /* Deinitialize EAL on iut_rpcs */
-    CHECK_RC(tapi_rte_eal_fini(&env, iut_rpcs));
+    /* Deinitialize EAL on iut_jobs_ctrl */
+    CHECK_RC(tapi_rte_eal_fini(&env, iut_jobs_ctrl));
 
     /* Set RPC provider to default value on IUT (do not use DPDK RPC server) */
     CHECK_RC(cfg_set_instance_fmt(CFG_VAL(STRING, "ta_rpcs"),

ts/perf/package.xml
@@ -31,7 +31,7 @@
 
         <prologue>
             <script name="perf_prologue"/>
-            <arg name="env" ref="env.peer2peer"/>
+            <arg name="env" ref="env.perf.peer2peer"/>
         </prologue>
 
         <!--- @autogroup -->

Xilinx-CNS/cns-dpdk-pmd-ts

commit 9631956ff382b2eb237c6c0288ca0b9e358fad7e

perf/perf_prologue: fix perf running with PCO reuse
If PCO reuse is enabled, iut_rpcs is preserved across tests and it
has EAL initialized. Destroy it to avoid failure because of the second
EAL init with the same prefix.

Fixes: c8ea5bf77775 ("perf/perf_prologue: use the same environment as tests")
Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>

ts/perf/perf_prologue.c
@@ -15,6 +15,7 @@
 
 #include "dpdk_pmd_test.h"
 
+#include "rcf_rpc.h"
 #include "tapi_cfg_cpu.h"
 #include "tapi_cfg_sys.h"
 #include "tapi_env.h"
@@ -124,6 +125,8 @@ main(int argc, char **argv)
     const struct if_nameindex *iut_port = NULL;
     struct tarpc_rte_eth_dev_info dev_info;
     const char *iut_job_control_rpcs = "iut_jobs_ctrl";
+    const char *iut_rpcs_name = "iut_rpcs";
+    rcf_rpc_server *iut_rpcs = NULL;
     struct tarpc_ether_addr iut_mac;
     char *iut_mac_str = NULL;
     tapi_cpu_index_t *indices = NULL;
@@ -184,9 +187,17 @@ main(int argc, char **argv)
 
     CFG_WAIT_CHANGES;
 
-    TEST_GET_PCO(iut_jobs_ctrl);
     TEST_GET_HOST(iut_host);
     TEST_GET_HOST(tst_host);
+
+    rc = rcf_rpc_server_get(iut_host->ta, iut_rpcs_name, NULL,
+            RCF_RPC_SERVER_GET_EXISTING | RCF_RPC_SERVER_GET_REUSE, &iut_rpcs);
+    if (rc == 0)
+        CHECK_RC(rcf_rpc_server_destroy(iut_rpcs));
+    else if (TE_RC_GET_ERROR(rc) != TE_ENOENT)
+        TEST_FAIL("Failed to get '%s' RPC server: %r", iut_rpcs_name, rc);
+
+    TEST_GET_PCO(iut_jobs_ctrl);
 
     /*
      * Make stats of SFC (Xilinx) NICs more consistent by removing update period